572bde62b60e0ea7ff5acea0be0053b9060b1688
mabur <magnus.burenius@gmail.com>
2018-09-30 20:24:24 +0200

    Refactor. Rename evaluating to interpreter

--- mang_lang/main.py
+++ mang_lang/main.py
@@ -1,6 +1,6 @@
-import evaluating
+from interpreter import interpret
 
 code = 'add(5, mul(8, 9))'
 print('Interpreting: {}'.format(code))
-value = evaluating.interpret(code)
+value = interpret(code)
 print('Value: {}'.format(value))
