babf0385039c6a0a418f9ae610bca901f6f87297
Magnus Burenius <magnus.burenius@mail.com>
2018-10-15 08:39:04 +0200

    Allow and ignore new lines in code input

--- mang_lang/lexing.py
+++ mang_lang/lexing.py
@@ -27,6 +27,7 @@ class Token:
 
 def lexer(code: str) -> Sequence[Token]:
     code = code.replace(' ', '')
+    code = code.replace('\n', '')
     num_characters = len(code)
     index = 0
     tokens = []
