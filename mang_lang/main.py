541ebed10edaf3ccc6539309ac881127b13cbfcd
Magnus Burenius <magnus.burenius@mail.com>
2018-10-18 17:41:28 +0200

    Pretty printing of json

--- mang_lang/main.py
+++ mang_lang/main.py
@@ -1,4 +1,5 @@
 from interpreter import interpret, create_abstract_syntax_tree_json
+import json
 
 code = '''
 (
@@ -9,6 +10,7 @@ result = f(8, f(2, 3))
 print('Interpreting: {}'.format(code))
 print('Abstract syntax tree:')
 ast = create_abstract_syntax_tree_json(code)
-print(ast)
+print(json.dumps(ast, indent=1))
+print('Interpreting: {}'.format(code))
 value = interpret(code)
 print('Value: {}'.format(value))
