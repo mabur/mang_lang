c3de9ef6659fc795cf89fb2219c9d33c5dc0f309
Magnus Burenius <magnus.burenius@mail.com>
2019-09-17 07:38:00 +0200

    Refactor. Extract function _lex_and_parse

--- mang_lang/interpreter.py
+++ mang_lang/interpreter.py
@@ -1,17 +1,18 @@
 from lexing import lexer
-from parsing import parse_expression
+from parsing import Expression, parse_expression
 from token_slice import TokenSlice
 import global_environment
 
 
 def create_abstract_syntax_tree_json(code: str):
-    tokens = lexer(code)
-    expression, _ = parse_expression(TokenSlice(tokens))
-    return expression.to_json()
+    return _lex_and_parse(code).to_json()
 
 
 def interpret(code: str):
+    return _lex_and_parse(code).evaluate(global_environment.ENVIRONMENT).to_json()
+
+
+def _lex_and_parse(code: str) -> Expression:
     tokens = lexer(code)
     expression, _ = parse_expression(TokenSlice(tokens))
-    value = expression.evaluate(global_environment.ENVIRONMENT).to_json()
-    return value
+    return expression
