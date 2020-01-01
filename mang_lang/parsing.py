590886ed4bfe91c066725b35fe4e84720ec878fa
Magnus Burenius <magnus.burenius@gmail.com>
2020-01-01 15:40:52 +0100

    Refactor. Rename "text" to "code"

--- mang_lang/parsing.py
+++ mang_lang/parsing.py
@@ -7,11 +7,11 @@ from error_handling import CodeFragment, print_syntax_error
 
 
 def parse(code: str) -> Expression:
-    text = CodeFragment(code)
+    code = CodeFragment(code)
     try:
-        return _parse_expression(text)[0]
+        return _parse_expression(code)[0]
     except:
-        print_syntax_error(text)
+        print_syntax_error(code)
         raise
 
 
@@ -38,146 +38,146 @@ LETTERS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_'
 WHITESPACE = ' \n'
 
 
-def _parse_keyword(text: CodeFragment, keyword: str) -> Tuple[str, CodeFragment]:
+def _parse_keyword(code: CodeFragment, keyword: str) -> Tuple[str, CodeFragment]:
     value = ''
     for expected in keyword:
-        actual = text.pop()
+        actual = code.pop()
         assert expected == actual
         value += actual
-    text = _parse_optional_white_space(text)
-    return value, text
+    code = _parse_optional_white_space(code)
+    return value, code
 
 
-def _parse_while(text: CodeFragment, predicate) -> Tuple[str, CodeFragment]:
+def _parse_while(code: CodeFragment, predicate) -> Tuple[str, CodeFragment]:
     value = ''
-    while text and predicate(text.front()):
-        value += text.pop()
-    return value, text
+    while code and predicate(code.front()):
+        value += code.pop()
+    return value, code
 
 
-def _parse_optional_white_space(text: CodeFragment) -> CodeFragment:
-    value, text = _parse_while(text, lambda c: c in WHITESPACE)
-    return text
+def _parse_optional_white_space(code: CodeFragment) -> CodeFragment:
+    value, code = _parse_while(code, lambda c: c in WHITESPACE)
+    return code
 
 
-def _parse_symbol(text: CodeFragment) -> Tuple[str, CodeFragment]:
-    value, text = _parse_while(text, lambda c: c in LETTERS)
+def _parse_symbol(code: CodeFragment) -> Tuple[str, CodeFragment]:
+    value, code = _parse_while(code, lambda c: c in LETTERS)
     assert value
-    text = _parse_optional_white_space(text)
-    return value, text
+    code = _parse_optional_white_space(code)
+    return value, code
 
 
-def _parse_number(text: CodeFragment) -> Tuple[Number, CodeFragment]:
-    section = copy.copy(text)
-    value, text = _parse_while(text, lambda c: c in DIGITS)
+def _parse_number(code: CodeFragment) -> Tuple[Number, CodeFragment]:
+    section = copy.copy(code)
+    value, code = _parse_while(code, lambda c: c in DIGITS)
     assert value
-    text = _parse_optional_white_space(text)
-    return Number(value, section=section), text
+    code = _parse_optional_white_space(code)
+    return Number(value, section=section), code
 
 
-def _parse_string(text: CodeFragment) -> Tuple[String, CodeFragment]:
-    section = copy.copy(text)
-    value, text = _parse_keyword(text, STRING_BEGIN)
-    body, text = _parse_while(text, lambda c: c != STRING_END)
+def _parse_string(code: CodeFragment) -> Tuple[String, CodeFragment]:
+    section = copy.copy(code)
+    value, code = _parse_keyword(code, STRING_BEGIN)
+    body, code = _parse_while(code, lambda c: c != STRING_END)
     value += body
-    value += text.pop()
-    text = _parse_optional_white_space(text)
-    return String(value, section=section), text
+    value += code.pop()
+    code = _parse_optional_white_space(code)
+    return String(value, section=section), code
 
 
-def _parse_array(text: CodeFragment) -> Tuple[Array, CodeFragment]:
-    section = copy.copy(text)
+def _parse_array(code: CodeFragment) -> Tuple[Array, CodeFragment]:
+    section = copy.copy(code)
     expressions = ()
-    _, text = _parse_keyword(text, ARRAY_BEGIN)
-    while not text.startswith(ARRAY_END):
-        expression, text = _parse_expression(text)
+    _, code = _parse_keyword(code, ARRAY_BEGIN)
+    while not code.startswith(ARRAY_END):
+        expression, code = _parse_expression(code)
         expressions += (expression,)
-        if text.startswith(COMMA):
-            _, text = _parse_keyword(text, COMMA)
-    _, text = _parse_keyword(text, ARRAY_END)
-    return Array(expressions=expressions, section=section), text
+        if code.startswith(COMMA):
+            _, code = _parse_keyword(code, COMMA)
+    _, code = _parse_keyword(code, ARRAY_END)
+    return Array(expressions=expressions, section=section), code
 
 
-def _parse_dictionary(text: CodeFragment) -> Tuple[Dictionary, CodeFragment]:
-    section = copy.copy(text)
+def _parse_dictionary(code: CodeFragment) -> Tuple[Dictionary, CodeFragment]:
+    section = copy.copy(code)
     variable_definitions = []
-    _, text = _parse_keyword(text, DICTIONARY_BEGIN)
-    while not text.startswith(DICTIONARY_END):
-        variable_definition, text = _parse_variable_definition(text)
+    _, code = _parse_keyword(code, DICTIONARY_BEGIN)
+    while not code.startswith(DICTIONARY_END):
+        variable_definition, code = _parse_variable_definition(code)
         variable_definitions.append(variable_definition)
-        if text.startswith(COMMA):
-            _, text = _parse_keyword(text, COMMA)
-    _, text = _parse_keyword(text, DICTIONARY_END)
-    return Dictionary(expressions=variable_definitions, section=section), text
+        if code.startswith(COMMA):
+            _, code = _parse_keyword(code, COMMA)
+    _, code = _parse_keyword(code, DICTIONARY_END)
+    return Dictionary(expressions=variable_definitions, section=section), code
 
 
-def _parse_lookup(text: CodeFragment) -> Tuple[Lookup, CodeFragment]:
-    section = copy.copy(text)
-    value, text = _parse_symbol(text)
-    if not text.startswith(OF):
-        return Lookup(left=value, right=None, section=section), text
-    _, text = _parse_keyword(text, OF)
-    right, text = _parse_expression(text)
-    return Lookup(left=value, right=right, section=section), text
+def _parse_lookup(code: CodeFragment) -> Tuple[Lookup, CodeFragment]:
+    section = copy.copy(code)
+    value, code = _parse_symbol(code)
+    if not code.startswith(OF):
+        return Lookup(left=value, right=None, section=section), code
+    _, code = _parse_keyword(code, OF)
+    right, code = _parse_expression(code)
+    return Lookup(left=value, right=right, section=section), code
 
 
-def _parse_variable_definition(text: CodeFragment) -> Tuple[VariableDefinition, CodeFragment]:
-    section = copy.copy(text)
-    value, text = _parse_symbol(text)
-    _, text = _parse_keyword(text, EQUAL)
-    expression, text = _parse_expression(text)
-    return VariableDefinition(name=value, expression=expression, section=section), text
+def _parse_variable_definition(code: CodeFragment) -> Tuple[VariableDefinition, CodeFragment]:
+    section = copy.copy(code)
+    value, code = _parse_symbol(code)
+    _, code = _parse_keyword(code, EQUAL)
+    expression, code = _parse_expression(code)
+    return VariableDefinition(name=value, expression=expression, section=section), code
 
 
-def _parse_function(text: CodeFragment) -> Tuple[Function, CodeFragment]:
-    section = copy.copy(text)
-    _, text = _parse_keyword(text, FROM)
-    value, text = _parse_symbol(text)
-    _, text = _parse_keyword(text, TO)
-    expression, text = _parse_expression(text)
-    return Function(argument_name=value, expression=expression, section=section), text
+def _parse_function(code: CodeFragment) -> Tuple[Function, CodeFragment]:
+    section = copy.copy(code)
+    _, code = _parse_keyword(code, FROM)
+    value, code = _parse_symbol(code)
+    _, code = _parse_keyword(code, TO)
+    expression, code = _parse_expression(code)
+    return Function(argument_name=value, expression=expression, section=section), code
 
 
-def _parse_conditional(text: CodeFragment) -> Tuple[Conditional, CodeFragment]:
-    section = copy.copy(text)
-    _, text = _parse_keyword(text, IF)
-    condition, text = _parse_expression(text)
-    _, text = _parse_keyword(text, THEN)
-    then_expression, text = _parse_expression(text)
-    _, text = _parse_keyword(text, ELSE)
-    else_expression, text = _parse_expression(text)
+def _parse_conditional(code: CodeFragment) -> Tuple[Conditional, CodeFragment]:
+    section = copy.copy(code)
+    _, code = _parse_keyword(code, IF)
+    condition, code = _parse_expression(code)
+    _, code = _parse_keyword(code, THEN)
+    then_expression, code = _parse_expression(code)
+    _, code = _parse_keyword(code, ELSE)
+    else_expression, code = _parse_expression(code)
     return (Conditional(condition=condition, then_expression=then_expression,
-                        else_expression=else_expression, section=section), text)
+                        else_expression=else_expression, section=section), code)
 
 
-def _parse_array_comprehension(text: CodeFragment)\
+def _parse_array_comprehension(code: CodeFragment)\
         -> Tuple[ArrayComprehension, CodeFragment]:
-    section = copy.copy(text)
-    _, text = _parse_keyword(text, EACH)
-    all_expression, text = _parse_expression(text)
-    _, text = _parse_keyword(text, FOR)
-    for_expression, text = _parse_expression(text)
-    _, text = _parse_keyword(text, IN)
-    in_expression, text = _parse_expression(text)
+    section = copy.copy(code)
+    _, code = _parse_keyword(code, EACH)
+    all_expression, code = _parse_expression(code)
+    _, code = _parse_keyword(code, FOR)
+    for_expression, code = _parse_expression(code)
+    _, code = _parse_keyword(code, IN)
+    in_expression, code = _parse_expression(code)
     if_expression = None
-    if text.startswith(IF):
-        _, text = _parse_keyword(text, IF)
-        if_expression, text = _parse_expression(text)
+    if code.startswith(IF):
+        _, code = _parse_keyword(code, IF)
+        if_expression, code = _parse_expression(code)
     return (ArrayComprehension(all_expression=all_expression,
                                for_expression=for_expression,
                                in_expression=in_expression,
                                if_expression=if_expression,
-                               section=section), text)
+                               section=section), code)
 
 
-def _parse_expression(text: CodeFragment) -> Tuple[Expression, CodeFragment]:
-    text = _parse_optional_white_space(text)
+def _parse_expression(code: CodeFragment) -> Tuple[Expression, CodeFragment]:
+    code = _parse_optional_white_space(code)
     try:
         for sequence, parser in _PARSE_TABLE:
-            if text.startswith(sequence):
-                return parser(text)
+            if code.startswith(sequence):
+                return parser(code)
     except KeyError:
-        raise ValueError('Bad token pattern: {}'.format(text[0]))
+        raise ValueError('Bad token pattern: {}'.format(code[0]))
 
 
 def _make_parse_table()\
