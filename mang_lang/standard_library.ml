df4ed74590bc9c710a5929424304efed7c02ed5c
Magnus Burenius <magnus.burenius@gmail.com>
2020-11-14 23:46:59 +0100

    refactor: formatting of standard_library.ml

--- mang_lang/standard_library.ml
+++ mang_lang/standard_library.ml
@@ -1,18 +1,18 @@
 {
     find = from input to
         if is_empty list<input then
-            [[],[]]
+            [[], []]
+        else if check_equality [first list<input, query<input] then
+            [[], list<input]
         else
-            if check_equality [first list<input, query<input]
-                then [[], list<input]
-                else result<{
-                    list = list<input,
-                    query = query<input,
-                    recursive_result = find {list=last_part list, query=query}
-                    head = concat [[first list], first recursive_result],
-                    tail = last recursive_result,
-                    result = [head, tail]
-                },
+            result<{
+                list = list<input,
+                query = query<input,
+                recursive_result = find {list = last_part list, query = query}
+                head = concat [[first list], first recursive_result],
+                tail = last recursive_result,
+                result = [head, tail]
+            },
     map = from input to
         if is_empty list<input then
             []
@@ -21,7 +21,7 @@
                 list = list<input,
                 f = f<input,
                 head = f first list,
-                tail = map {list=last_part list, f=f},
-                result = concat[[head], tail]
+                tail = map {list = last_part list, f = f},
+                result = concat [[head], tail]
             }
 }
