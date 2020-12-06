4a0a9fa8b635fe419eef923162cd7718a1116dfd
Magnus Burenius <magnus.burenius@gmail.com>
2020-12-06 18:07:42 +0100

    refactor: extract module for tests of equality

---
+++ mang_lang/tests/test_equality.py
@@ -0,0 +1,71 @@
+from typing import Any, Mapping
+import unittest
+from interpreter import interpret
+
+
+def V(value) -> Mapping[str, Any]:
+    return {"type": "number", "value": value}
+
+
+def S(value) -> Mapping[str, Any]:
+    return {"type": "string", "value": value}
+
+
+class TestEqualNumbers(unittest.TestCase):
+    def test0(self):
+        self.assertEqual(V(1), interpret('equal [1, 1]'))
+
+    def test1(self):
+        self.assertEqual(V(0), interpret('equal [0, 1]'))
+
+    def test2(self):
+        self.assertEqual(V(0), interpret('equal [1, 0]'))
+
+    def test3(self):
+        self.assertEqual(V(1), interpret('equal [0, 0]'))
+
+
+class TestEqualStrings(unittest.TestCase):
+    def test0(self):
+        self.assertEqual(V(1), interpret('equal ["b", "b"]'))
+
+    def test1(self):
+        self.assertEqual(V(0), interpret('equal ["a", "b"]'))
+
+    def test2(self):
+        self.assertEqual(V(0), interpret('equal ["b", "a"]'))
+
+    def test3(self):
+        self.assertEqual(V(1), interpret('equal ["a", "a"]'))
+
+
+class TestUnequalNumbers(unittest.TestCase):
+    def test0(self):
+        self.assertEqual(V(0), interpret('unequal [1, 1]'))
+
+    def test1(self):
+        self.assertEqual(V(1), interpret('unequal [0, 1]'))
+
+    def test2(self):
+        self.assertEqual(V(1), interpret('unequal [1, 0]'))
+
+    def test3(self):
+        self.assertEqual(V(0), interpret('unequal [0, 0]'))
+
+
+class TestUnequalStrings(unittest.TestCase):
+    def test0(self):
+        self.assertEqual(V(0), interpret('unequal ["b", "b"]'))
+
+    def test1(self):
+        self.assertEqual(V(1), interpret('unequal ["a", "b"]'))
+
+    def test2(self):
+        self.assertEqual(V(1), interpret('unequal ["b", "a"]'))
+
+    def test3(self):
+        self.assertEqual(V(0), interpret('unequal ["a", "a"]'))
+
+
+if __name__ == '__main__':
+    unittest.main()
--- mang_lang/tests/tests.py
+++ mang_lang/tests/tests.py
@@ -45,63 +45,6 @@ class TestRecursion(unittest.TestCase):
         self.assertEqual(V(3628800), interpret(code))
 
 
-
-class TestEqual(unittest.TestCase):
-    def test0(self):
-        self.assertEqual(V(1), interpret('equal [1, 1]'))
-
-    def test1(self):
-        self.assertEqual(V(0), interpret('equal [0, 1]'))
-
-    def test2(self):
-        self.assertEqual(V(0), interpret('equal [1, 0]'))
-
-    def test3(self):
-        self.assertEqual(V(1), interpret('equal [0, 0]'))
-
-
-class TestEqualString(unittest.TestCase):
-    def test0(self):
-        self.assertEqual(V(1), interpret('equal ["b", "b"]'))
-
-    def test1(self):
-        self.assertEqual(V(0), interpret('equal ["a", "b"]'))
-
-    def test2(self):
-        self.assertEqual(V(0), interpret('equal ["b", "a"]'))
-
-    def test3(self):
-        self.assertEqual(V(1), interpret('equal ["a", "a"]'))
-
-
-class TestUnequal(unittest.TestCase):
-    def test0(self):
-        self.assertEqual(V(0), interpret('unequal [1, 1]'))
-
-    def test1(self):
-        self.assertEqual(V(1), interpret('unequal [0, 1]'))
-
-    def test2(self):
-        self.assertEqual(V(1), interpret('unequal [1, 0]'))
-
-    def test3(self):
-        self.assertEqual(V(0), interpret('unequal [0, 0]'))
-
-
-class TestUnequalString(unittest.TestCase):
-    def test0(self):
-        self.assertEqual(V(0), interpret('unequal ["b", "b"]'))
-
-    def test1(self):
-        self.assertEqual(V(1), interpret('unequal ["a", "b"]'))
-
-    def test2(self):
-        self.assertEqual(V(1), interpret('unequal ["b", "a"]'))
-
-    def test3(self):
-        self.assertEqual(V(0), interpret('unequal ["a", "a"]'))
-
-
 class TestExpression(unittest.TestCase):
     def test_composition(self):
         self.assertEqual(V(12), interpret('add [sub [5, 3], mul [2, 5]]'))
