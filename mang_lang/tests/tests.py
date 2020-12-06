e2f789a72cab8793c8f8ed7eb535bb326d3e0d62
Magnus Burenius <magnus.burenius@gmail.com>
2020-12-06 17:49:03 +0100

    refactor: extract module for tests of number functions

--- mang_lang/tests/tests.py
+++ mang_lang/tests/tests.py
@@ -29,18 +29,6 @@ class TestArray(unittest.TestCase):
 
 
 class TestBuiltinFunctions(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(V(8), interpret('add [5, 3]'))
-
-    def test_sub(self):
-        self.assertEqual(V(2), interpret('sub [5, 3]'))
-
-    def test_mul(self):
-        self.assertEqual(V(15), interpret('mul [5, 3]'))
-
-    def test_div(self):
-        self.assertEqual(V(2), interpret('div [6, 3]'))
-
     def test_if_then_else_true(self):
         self.assertEqual(V(1), interpret('if 1 then 1 else 2'))
 
@@ -311,72 +299,6 @@ class TestUnequalString(unittest.TestCase):
         self.assertEqual(V(0), interpret('unequal ["a", "a"]'))
 
 
-class TestIncreasing(unittest.TestCase):
-    def test0(self):
-        self.assertEqual(V(0), interpret('increasing [0, 0]'))
-
-    def test1(self):
-        self.assertEqual(V(1), interpret('increasing [0, 1]'))
-
-    def test2(self):
-        self.assertEqual(V(0), interpret('increasing [1, 1]'))
-
-    def test3(self):
-        self.assertEqual(V(0), interpret('increasing [1, 0]'))
-
-
-class TestWeaklyIncreasing(unittest.TestCase):
-    def test0(self):
-        self.assertEqual(V(1), interpret('weakly_increasing [0, 0]'))
-
-    def test1(self):
-        self.assertEqual(V(1), interpret('weakly_increasing [0, 1]'))
-
-    def test2(self):
-        self.assertEqual(V(1), interpret('weakly_increasing [1, 1]'))
-
-    def test3(self):
-        self.assertEqual(V(0), interpret('weakly_increasing [1, 0]'))
-
-
-class TestDecreasing(unittest.TestCase):
-    def test0(self):
-        self.assertEqual(V(0), interpret('decreasing [0, 0]'))
-
-    def test1(self):
-        self.assertEqual(V(0), interpret('decreasing [0, 1]'))
-
-    def test2(self):
-        self.assertEqual(V(0), interpret('decreasing [1, 1]'))
-
-    def test3(self):
-        self.assertEqual(V(1), interpret('decreasing [1, 0]'))
-
-
-class TestWeaklyDecreasing(unittest.TestCase):
-    def test0(self):
-        self.assertEqual(V(1), interpret('weakly_decreasing [0, 0]'))
-
-    def test1(self):
-        self.assertEqual(V(0), interpret('weakly_decreasing [0, 1]'))
-
-    def test2(self):
-        self.assertEqual(V(1), interpret('weakly_decreasing [1, 1]'))
-
-    def test3(self):
-        self.assertEqual(V(1), interpret('weakly_decreasing [1, 0]'))
-
-
-class TestaddMinMax(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(V(6), interpret('add [1,2,3]'))
-
-    def test_min(self):
-        self.assertEqual(V(1), interpret('min [1,2,3]'))
-
-    def test_max(self):
-        self.assertEqual(V(3), interpret('max [1,2,3]'))
-
 
 class TestExpression(unittest.TestCase):
     def test_composition(self):
@@ -556,25 +478,6 @@ class TestImport(unittest.TestCase):
 
     def test3(self):
         self.assertEqual(V(9), interpret('b<{a = import "tests/test_data/test3.ml", square = square<a, b=square 3}'))
-
-
-class TestSqrt(unittest.TestCase):
-    def test4(self):
-        self.assertEqual(V(2), interpret('sqrt 4'))
-
-    def test10(self):
-        self.assertEqual(V(10), interpret('sqrt 100'))
-
-
-class TestAbs(unittest.TestCase):
-    def test0(self):
-        self.assertEqual(V(1), interpret('abs -1'))
-
-    def test1(self):
-        self.assertEqual(V(0), interpret('abs 0'))
-
-    def test2(self):
-        self.assertEqual(V(1), interpret('abs 1'))
 
 
 class TestErrorMessages(unittest.TestCase):
