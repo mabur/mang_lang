363f6aeb20fd8f18462e626447045e3a6e372046
Magnus Burenius <magnus.burenius@gmail.com>
2021-04-05 13:09:55 +0200

    Make Character::isString final

--- mang_lang_cpp/expressions/Character.h
+++ mang_lang_cpp/expressions/Character.h
@@ -9,6 +9,6 @@ struct Character : public Expression {
     ExpressionPointer evaluate(const Expression* parent, std::ostream& log) const final;
     static ExpressionPointer parse(CodeRange code);
     static bool startsWith(CodeRange code);
-    bool isEqual(const Expression* expression) const;
+    bool isEqual(const Expression* expression) const final;
     char character() const final;
 };
