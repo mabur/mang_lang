a1e4c76a5bbf049dc80fdf3509548100cd95543f
Magnus Burenius <magnus.burenius@gmail.com>
2021-05-15 13:49:49 +0200

    refactor: move DictionaryElement to its own file

--- mang_lang_cpp/expressions/Dictionary.h
+++ mang_lang_cpp/expressions/Dictionary.h
@@ -1,49 +1,7 @@
 #pragma once
+#include "DictionaryElement.h"
 #include "Expression.h"
 #include "Name.h"
-
-struct DictionaryElement : Expression {
-    DictionaryElement(
-        CodeRange range,
-        const Expression* parent,
-        const Name& name,
-        ExpressionPointer expression
-    )
-        : Expression{range, parent}, name{name}, expression{std::move(expression)}
-    {}
-    Name name;
-    ExpressionPointer expression;
-    std::string serialize() const final {
-        return name.serialize() + '=' + expression->serialize() + ',';
-    }
-    ExpressionPointer evaluate(const Expression*, std::ostream&) const final {
-        return {};
-    }
-    ExpressionPointer lookup(const std::string& s) const final {
-        if (name.value == s) {
-            return expression;
-        }
-        return nullptr;
-    };
-    static DictionaryElement parse(CodeRange code) {
-        auto first = code.begin();
-        code = parseWhiteSpace(code);
-        throwIfEmpty(code);
-        const auto name = Name::parse(code);
-        code.first = name.end();
-        code = parseWhiteSpace(code);
-        code = parseCharacter(code, '=');
-        code = parseWhiteSpace(code);
-        auto expression = Expression::parse(code);
-        code.first = expression->end();
-        code = parseWhiteSpace(code);
-        code = parseOptionalCharacter(code, ',');
-        return DictionaryElement(CodeRange{first, code.first}, nullptr, name, std::move(expression));
-    }
-    static bool startsWith(CodeRange) {
-        return false;
-    }
-};
 
 struct Dictionary : public Expression {
     Dictionary(CodeRange range, const Expression* parent)
---
+++ mang_lang_cpp/expressions/DictionaryElement.cpp
@@ -0,0 +1,1 @@
+#include "DictionaryElement.h"
