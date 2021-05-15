89338b5468c287f1186c00b542090a415fae3893
Magnus Burenius <magnus.burenius@gmail.com>
2021-05-15 13:52:51 +0200

    refactor: move implementation of DictionaryElement to cpp

--- mang_lang_cpp/expressions/DictionaryElement.cpp
+++ mang_lang_cpp/expressions/DictionaryElement.cpp
@@ -1,1 +1,49 @@
 #include "DictionaryElement.h"
+#include "Expression.h"
+#include "Name.h"
+
+DictionaryElement::DictionaryElement(
+    CodeRange range,
+    const Expression* parent,
+    const Name& name,
+    ExpressionPointer expression
+)
+    : Expression{range, parent}, name{name}, expression{std::move(expression)}
+{}
+
+std::string DictionaryElement::serialize() const {
+    return name.serialize() + '=' + expression->serialize() + ',';
+}
+
+ExpressionPointer DictionaryElement::evaluate(const Expression*, std::ostream&) const {
+    return {};
+}
+
+ExpressionPointer DictionaryElement::lookup(const std::string& s) const {
+    if (name.value == s) {
+        return expression;
+    }
+    return nullptr;
+}
+
+DictionaryElement DictionaryElement::parse(CodeRange code) {
+    auto first = code.begin();
+    code = parseWhiteSpace(code);
+    throwIfEmpty(code);
+    const auto name = Name::parse(code);
+    code.first = name.end();
+    code = parseWhiteSpace(code);
+    code = parseCharacter(code, '=');
+    code = parseWhiteSpace(code);
+    auto expression = Expression::parse(code);
+    code.first = expression->end();
+    code = parseWhiteSpace(code);
+    code = parseOptionalCharacter(code, ',');
+    return DictionaryElement(
+        CodeRange{first, code.first}, nullptr, name, std::move(expression)
+    );
+}
+
+bool DictionaryElement::startsWith(CodeRange) {
+    return false;
+}
