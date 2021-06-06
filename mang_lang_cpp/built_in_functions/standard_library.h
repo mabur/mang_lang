559251b56cedaf155df9c6f6059a82d60c30768c
Magnus Burenius <magnus.burenius@gmail.com>
2021-06-06 17:39:08 +0200

    reformat standard library

--- mang_lang_cpp/built_in_functions/standard_library.h
+++ mang_lang_cpp/built_in_functions/standard_library.h
@@ -17,114 +17,117 @@ const std::string STANDARD_LIBRARY = R"(
     fifth = in x out first!rest!rest!rest!rest!x
 
     get_index = in (index list) out result@{
-            i = 0
-            tail = list
-            while not!equal!(i index)
-                tail = rest!tail
-                i = inc!i
-            end
-            result = first!tail
-        }
+        i = 0
+        tail = list
+        while not!equal!(i index)
+            tail = rest!tail
+            i = inc!i
+        end
+        result = first!tail
+    }
 
     reverse = in list out result@{
-            result = empty!list
-            tail = list
-            while tail
-                result = prepend!(first!tail result)
-                tail = rest!tail
-            end
-        }
+        result = empty!list
+        tail = list
+        while tail
+            result = prepend!(first!tail result)
+            tail = rest!tail
+        end
+    }
 
     concat = in (left right) out result@{
-            result = right
-            tail = reverse!left
-            while tail
-                result = prepend!(first!tail result)
-                tail = rest!tail
-            end
-        }
+        result = right
+        tail = reverse!left
+        while tail
+            result = prepend!(first!tail result)
+            tail = rest!tail
+        end
+    }
 
     range = in x out result@{
-            result = ()
-            y = x
-            while y
-                result = prepend!(dec!y result)
-                y = dec!y
-            end
-        }
+        result = ()
+        y = x
+        while y
+            result = prepend!(dec!y result)
+            y = dec!y
+        end
+    }
 
     enumerate = in list out result@{
-            reversed_result = ()
-            tail = list
-            index = 0
-            while tail
-                reversed_result = prepend!({index=index item=first!tail} reversed_result)
-                tail = rest!tail
-                index = inc!index
-            end
-            result = reverse!reversed_result
-        }
+        reversed_result = ()
+        tail = list
+        index = 0
+        while tail
+            reversed_result = prepend!(
+                {index=index item=first!tail}
+                reversed_result
+            )
+            tail = rest!tail
+            index = inc!index
+        end
+        result = reverse!reversed_result
+    }
 
     map = in (f list) out result@{
-            reversed_result = empty!list
-            tail = list
-            while tail
-                reversed_result = prepend!(f!first!tail reversed_result)
-                tail = rest!tail
-            end
-            result = reverse!reversed_result
-        }
+        reversed_result = empty!list
+        tail = list
+        while tail
+            reversed_result = prepend!(f!first!tail reversed_result)
+            tail = rest!tail
+        end
+        result = reverse!reversed_result
+    }
 
     filter = in (predicate list) out result@{
-            reversed_result = empty!list
-            tail = list
-            while tail
-                reversed_result =
-                    if predicate!first!tail then
-                        prepend!(first!tail reversed_result)
-                    else
-                        reversed_result
-                tail = rest!tail
-            end
-            result = reverse!reversed_result
-        }
+        reversed_result = empty!list
+        tail = list
+        while tail
+            reversed_result =
+                if predicate!first!tail then
+                    prepend!(first!tail reversed_result)
+                else
+                    reversed_result
+            tail = rest!tail
+        end
+        result = reverse!reversed_result
+    }
 
     replace = in (old_item new_item list) out
         map!(in item out if equal!(item old_item) then new_item else item list)
 
     count = in list out result@{
-            result = 0
-            tail = list
-            while tail
-                result = inc!result
-                tail = rest!tail
-            end
-        }
+        result = 0
+        tail = list
+        while tail
+            result = inc!result
+            tail = rest!tail
+        end
+    }
 
     count_item = in (item list) out result@{
-            result = 0
-            tail = list
-            while tail
-                result =
-                    if equal!(first!tail item) then
-                        inc!result
-                    else
-                        result
-                tail = rest!tail
-            end
-        }
+        result = 0
+        tail = list
+        while tail
+            result =
+                if equal!(first!tail item) then
+                    inc!result
+                else
+                    result
+            tail = rest!tail
+        end
+    }
 
     count_if = in (predicate list) out result@{
-            result = 0
-            tail = list
-            while tail
-                result =
-                    if predicate!first!tail then
-                        inc!result
-                    else
-                        result
-                tail = rest!tail
-            end
-        }
+        result = 0
+        tail = list
+        while tail
+            result =
+                if predicate!first!tail then
+                    inc!result
+                else
+                    result
+            tail = rest!tail
+        end
+    }
 }
 )";
