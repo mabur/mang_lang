82c583251c3334958c9b5c9099dfe9f621a26a29
Magnus Burenius <magnus.burenius@gmail.com>
2021-07-11 12:55:29 +0200

    refactor: remove forward declarations in evaluation.cpp

--- mang_lang_cpp/operations/evaluate.cpp
+++ mang_lang_cpp/operations/evaluate.cpp
@@ -15,19 +15,6 @@
 #include "../expressions/Number.h"
 #include "../expressions/String.h"
 
-ExpressionPointer evaluateCharacter(const Character& character, const Expression* environment, std::ostream& log);
-ExpressionPointer evaluateConditional(const Conditional& conditional, const Expression* environment, std::ostream& log);
-ExpressionPointer evaluateDictionary(const Dictionary& dictionary, const Expression* environment, std::ostream& log);
-ExpressionPointer evaluateFunction(const Function& function, const Expression* environment, std::ostream& log);
-ExpressionPointer evaluateFunctionDictionary(const FunctionDictionary& function_dictionary, const Expression* environment, std::ostream& log);
-ExpressionPointer evaluateFunctionList(const FunctionList& function_list, const Expression* environment, std::ostream& log);
-ExpressionPointer evaluateList(const List& list, const Expression* environment, std::ostream& log);
-ExpressionPointer evaluateLookupChild(const LookupChild& lookup_child, const Expression* environment, std::ostream& log);
-ExpressionPointer evaluateLookupFunction(const LookupFunction& lookup_function, const Expression* environment, std::ostream& log);
-ExpressionPointer evaluateLookupSymbol(const LookupSymbol& lookup_symbol, const Expression* environment, std::ostream& log);
-ExpressionPointer evaluateNumber(const Number& number, const Expression* environment, std::ostream& log);
-ExpressionPointer evaluateString(const String& string, const Expression* environment, std::ostream& log);
-
 ExpressionPointer evaluateCharacter(
     const Character& character, const Expression* environment, std::ostream& log
 ) {
