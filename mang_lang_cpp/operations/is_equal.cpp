1213455048972986c26acab50680d0943ea0a4f9
Magnus Burenius <magnus.burenius@gmail.com>
2021-07-28 18:02:45 +0200

    refactor: inline isEqualCharacter

--- mang_lang_cpp/operations/is_equal.cpp
+++ mang_lang_cpp/operations/is_equal.cpp
@@ -1,18 +1,8 @@
 #include "is_equal.h"
 
-#include "../expressions/Character.h"
-
 #include "character.h"
 #include "list.h"
 #include "number.h"
-
-bool isEqualCharacter(const Character* character, const Expression* expression) {
-    try {
-        return ::character(character) == ::character(expression);
-    } catch (...) {
-        return false;
-    }
-}
 
 bool isEqualList(const ExpressionPointer& left_smart, const ExpressionPointer& right_smart) {
     auto left = list(left_smart.get());
@@ -36,7 +26,11 @@ bool isEqual(const ExpressionPointer& left_smart, const ExpressionPointer& right
         }
     }
     if (left->type_ == CHARACTER) {
-        return isEqualCharacter(dynamic_cast<const Character *>(left), right);
+        try {
+            return ::character(left) == ::character(right);
+        } catch (...) {
+            return false;
+        }
     }
     if (left->type_ == LIST) {
         return isEqualList(left_smart, right_smart);
