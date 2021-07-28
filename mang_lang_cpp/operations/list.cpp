fb946c25f7d23ff6958a12aa9a82dc383da95048
Magnus Burenius <magnus.burenius@gmail.com>
2021-07-28 19:08:13 +0200

    refactor: list operation

--- mang_lang_cpp/operations/list.cpp
+++ mang_lang_cpp/operations/list.cpp
@@ -3,21 +3,13 @@
 #include "../expressions/List.h"
 #include "../expressions/String.h"
 
-const InternalList& listList(const List* list) {
-    return list->elements;
-}
-
-const InternalList& listString(const String* string) {
-    return string->elements;
-}
-
 const InternalList& list(const ExpressionPointer& expression_smart) {
     const auto expression = expression_smart.get();
     if (expression->type_ == LIST) {
-        return listList(dynamic_cast<const List *>(expression));
+        return dynamic_cast<const List *>(expression)->elements;
     }
     if (expression->type_ == STRING) {
-        return listString(dynamic_cast<const String *>(expression));
+        return dynamic_cast<const String *>(expression)->elements;
     }
     throw std::runtime_error{"Expected list"};
 }
