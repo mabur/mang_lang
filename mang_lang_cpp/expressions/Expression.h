777623ebaca8b83814f7988dfcd6ee062860f4ab
Magnus Burenius <magnus.burenius@gmail.com>
2021-07-30 15:46:48 +0200

    refactor: move enum ExpressionType

--- mang_lang_cpp/expressions/Expression.h
+++ mang_lang_cpp/expressions/Expression.h
@@ -11,26 +11,6 @@
 
 #include "ExpressionPointer.h"
 
-enum ExpressionType {
-    CHARACTER,
-    CONDITIONAL,
-    DICTIONARY,
-    FUNCTION,
-    FUNCTION_BUILT_IN,
-    FUNCTION_DICTIONARY,
-    FUNCTION_LIST,
-    LIST,
-    LOOKUP_CHILD,
-    LOOKUP_FUNCTION,
-    LOOKUP_SYMBOL,
-    NAME,
-    NAMED_ELEMENT,
-    WHILE_ELEMENT,
-    END_ELEMENT,
-    NUMBER,
-    STRING,
-};
-
 struct Expression;
 //using ExpressionPointer = std::shared_ptr<const Expression>;
 using InternalList = SinglyLinkedList<ExpressionPointer>;
--- mang_lang_cpp/expressions/ExpressionPointer.h
+++ mang_lang_cpp/expressions/ExpressionPointer.h
@@ -2,6 +2,26 @@
 
 #include <memory>
 
+enum ExpressionType {
+    CHARACTER,
+    CONDITIONAL,
+    DICTIONARY,
+    FUNCTION,
+    FUNCTION_BUILT_IN,
+    FUNCTION_DICTIONARY,
+    FUNCTION_LIST,
+    LIST,
+    LOOKUP_CHILD,
+    LOOKUP_FUNCTION,
+    LOOKUP_SYMBOL,
+    NAME,
+    NAMED_ELEMENT,
+    WHILE_ELEMENT,
+    END_ELEMENT,
+    NUMBER,
+    STRING,
+};
+
 struct Expression;
 
 struct ExpressionPointer {
