753f134e5c195e9ee7f877ee6ea9f37862962f04
Magnus Burenius <magnus.burenius@gmail.com>
2021-08-05 23:08:46 +0200

    refactor: remove dynamic casts in serialize

--- mang_lang_cpp/operations/serialize.cpp
+++ mang_lang_cpp/operations/serialize.cpp
@@ -153,24 +153,22 @@ std::string serializeString(const ExpressionPointer& string) {
 
 std::string serialize(const ExpressionPointer& expression_smart) {
     const auto type = expression_smart.type;
-    const auto index = expression_smart.index;
-    const auto expression = expression_smart.get();
     switch (type) {
-        case CHARACTER: return serializeCharacter(*dynamic_cast<const Character *>(expression));
-        case CONDITIONAL: return serializeConditional(*dynamic_cast<const Conditional *>(expression));
-        case DICTIONARY: return serializeDictionary(*dynamic_cast<const Dictionary *>(expression));
-        case NAMED_ELEMENT: return serializeDictionaryElement(ExpressionPointer{type, index});
-        case WHILE_ELEMENT: return serializeDictionaryElement(ExpressionPointer{type, index});
-        case END_ELEMENT: return serializeDictionaryElement(ExpressionPointer{type, index});
-        case FUNCTION: return serializeFunction(*dynamic_cast<const Function *>(expression));
-        case FUNCTION_DICTIONARY: return serializeFunctionDictionary(*dynamic_cast<const FunctionDictionary *>(expression));
-        case FUNCTION_LIST: return serializeFunctionList(*dynamic_cast<const FunctionList *>(expression));
+        case CHARACTER: return serializeCharacter(expression_smart.character());
+        case CONDITIONAL: return serializeConditional(expression_smart.conditional());
+        case DICTIONARY: return serializeDictionary(expression_smart.dictionary());
+        case NAMED_ELEMENT: return serializeDictionaryElement(expression_smart);
+        case WHILE_ELEMENT: return serializeDictionaryElement(expression_smart);
+        case END_ELEMENT: return serializeDictionaryElement(expression_smart);
+        case FUNCTION: return serializeFunction(expression_smart.function());
+        case FUNCTION_DICTIONARY: return serializeFunctionDictionary(expression_smart.functionDictionary());
+        case FUNCTION_LIST: return serializeFunctionList(expression_smart.functionList());
         case LIST: return serializeList(expression_smart);
-        case LOOKUP_CHILD: return serializeLookupChild(*dynamic_cast<const LookupChild *>(expression));
-        case LOOKUP_FUNCTION: return serializeLookupFunction(*dynamic_cast<const LookupFunction *>(expression));
-        case LOOKUP_SYMBOL: return serializeLookupSymbol(*dynamic_cast<const LookupSymbol *>(expression));
-        case NAME: return serializeName(*dynamic_cast<const Name *>(expression));
-        case NUMBER: return serializeNumber(*dynamic_cast<const Number *>(expression));
+        case LOOKUP_CHILD: return serializeLookupChild(expression_smart.lookupChild());
+        case LOOKUP_FUNCTION: return serializeLookupFunction(expression_smart.lookupFunction());
+        case LOOKUP_SYMBOL: return serializeLookupSymbol(expression_smart.lookupSymbol());
+        case NAME: return serializeName(expression_smart.name());
+        case NUMBER: return serializeNumber(expression_smart.number());
         case STRING: return serializeString(expression_smart);
         default: throw std::runtime_error{"Did not recognize expression to serializeCharacter: " + std::to_string(type)};
     }
