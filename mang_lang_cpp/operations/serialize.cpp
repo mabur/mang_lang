fcb1fc9527ea2173ec1f5a9ab7d82590eb428054
Magnus Burenius <magnus.burenius@gmail.com>
2021-08-05 23:00:21 +0200

    refactor: switch in serialize

--- mang_lang_cpp/operations/serialize.cpp
+++ mang_lang_cpp/operations/serialize.cpp
@@ -155,21 +155,23 @@ std::string serialize(const ExpressionPointer& expression_smart) {
     const auto type = expression_smart.type;
     const auto index = expression_smart.index;
     const auto expression = expression_smart.get();
-    if (type == CHARACTER) {return serializeCharacter(*dynamic_cast<const Character *>(expression));}
-    if (type == CONDITIONAL) {return serializeConditional(*dynamic_cast<const Conditional *>(expression));}
-    if (type == DICTIONARY) {return serializeDictionary(*dynamic_cast<const Dictionary *>(expression));}
-    if (type == NAMED_ELEMENT) {return serializeDictionaryElement(ExpressionPointer{type, index});}
-    if (type == WHILE_ELEMENT) {return serializeDictionaryElement(ExpressionPointer{type, index});}
-    if (type == END_ELEMENT) {return serializeDictionaryElement(ExpressionPointer{type, index});}
-    if (type == FUNCTION) {return serializeFunction(*dynamic_cast<const Function *>(expression));}
-    if (type == FUNCTION_DICTIONARY) {return serializeFunctionDictionary(*dynamic_cast<const FunctionDictionary *>(expression));}
-    if (type == FUNCTION_LIST) {return serializeFunctionList(*dynamic_cast<const FunctionList *>(expression));}
-    if (type == LIST) {return serializeList(expression_smart);}
-    if (type == LOOKUP_CHILD) {return serializeLookupChild(*dynamic_cast<const LookupChild *>(expression));}
-    if (type == LOOKUP_FUNCTION) {return serializeLookupFunction(*dynamic_cast<const LookupFunction *>(expression));}
-    if (type == LOOKUP_SYMBOL) {return serializeLookupSymbol(*dynamic_cast<const LookupSymbol *>(expression));}
-    if (type == NAME) {return serializeName(*dynamic_cast<const Name *>(expression));}
-    if (type == NUMBER) {return serializeNumber(*dynamic_cast<const Number *>(expression));}
-    if (type == STRING) {return serializeString(expression_smart);}
-    throw std::runtime_error{"Did not recognize expression to serializeCharacter: " + std::to_string(type)};
+    switch (type) {
+        case CHARACTER: return serializeCharacter(*dynamic_cast<const Character *>(expression));
+        case CONDITIONAL: return serializeConditional(*dynamic_cast<const Conditional *>(expression));
+        case DICTIONARY: return serializeDictionary(*dynamic_cast<const Dictionary *>(expression));
+        case NAMED_ELEMENT: return serializeDictionaryElement(ExpressionPointer{type, index});
+        case WHILE_ELEMENT: return serializeDictionaryElement(ExpressionPointer{type, index});
+        case END_ELEMENT: return serializeDictionaryElement(ExpressionPointer{type, index});
+        case FUNCTION: return serializeFunction(*dynamic_cast<const Function *>(expression));
+        case FUNCTION_DICTIONARY: return serializeFunctionDictionary(*dynamic_cast<const FunctionDictionary *>(expression));
+        case FUNCTION_LIST: return serializeFunctionList(*dynamic_cast<const FunctionList *>(expression));
+        case LIST: return serializeList(expression_smart);
+        case LOOKUP_CHILD: return serializeLookupChild(*dynamic_cast<const LookupChild *>(expression));
+        case LOOKUP_FUNCTION: return serializeLookupFunction(*dynamic_cast<const LookupFunction *>(expression));
+        case LOOKUP_SYMBOL: return serializeLookupSymbol(*dynamic_cast<const LookupSymbol *>(expression));
+        case NAME: return serializeName(*dynamic_cast<const Name *>(expression));
+        case NUMBER: return serializeNumber(*dynamic_cast<const Number *>(expression));
+        case STRING: return serializeString(expression_smart);
+        default: throw std::runtime_error{"Did not recognize expression to serializeCharacter: " + std::to_string(type)};
+    }
 }
