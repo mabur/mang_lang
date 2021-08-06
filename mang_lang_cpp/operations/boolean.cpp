b3c6b8afe2d7882779e602291774aae5c4360968
Magnus Burenius <magnus.burenius@gmail.com>
2021-08-06 12:05:00 +0200

    refactor: do static_cast in boolean operation

--- mang_lang_cpp/operations/boolean.cpp
+++ mang_lang_cpp/operations/boolean.cpp
@@ -4,14 +4,13 @@
 #include "../expressions/List.h"
 #include "../expressions/Number.h"
 #include "../expressions/String.h"
-#include "list.h"
 
 bool boolean(const ExpressionPointer& expression) {
     switch(expression.type) {
         case DICTIONARY: return !expression.dictionary().elements.empty();
-        case NUMBER: return expression.number().value;
-        case LIST: return !!::list(expression);
-        case STRING: return !!list(expression);
+        case NUMBER: return static_cast<bool>(expression.number().value);
+        case LIST: return static_cast<bool>(list(expression));
+        case STRING: return static_cast<bool>(list(expression));
         default: throw std::runtime_error{"Expected boolean"};
     }
 }
