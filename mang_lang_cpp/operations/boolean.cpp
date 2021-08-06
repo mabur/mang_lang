89c07ccd0bef0f0c9528b3aa7e4adaccd5fdb76b
Magnus Burenius <magnus.burenius@gmail.com>
2021-08-06 12:02:50 +0200

    refactor: bool operation

--- mang_lang_cpp/operations/boolean.cpp
+++ mang_lang_cpp/operations/boolean.cpp
@@ -6,28 +6,12 @@
 #include "../expressions/String.h"
 #include "list.h"
 
-bool booleanDictionary(const Dictionary* dictionary) {
-    return !dictionary->elements.empty();
-}
-
-bool booleanNumber(const Number* number) {
-    return static_cast<bool>(number->value);
-}
-
-bool boolean(const ExpressionPointer& expression_smart) {
-    const auto type = expression_smart.type;
-    const auto expression = expression_smart.get();
-    if (type == DICTIONARY) {
-        return booleanDictionary(dynamic_cast<const Dictionary *>(expression));
+bool boolean(const ExpressionPointer& expression) {
+    switch(expression.type) {
+        case DICTIONARY: return !expression.dictionary().elements.empty();
+        case NUMBER: return expression.number().value;
+        case LIST: return !!::list(expression);
+        case STRING: return !!list(expression);
+        default: throw std::runtime_error{"Expected boolean"};
     }
-    if (type == NUMBER) {
-        return booleanNumber(dynamic_cast<const Number *>(expression));
-    }
-    if (type == LIST) {
-        return !!::list(expression_smart);
-    }
-    if (type == STRING) {
-        return !!list(expression_smart);
-    }
-    throw std::runtime_error{"Expected boolean"};
 }
