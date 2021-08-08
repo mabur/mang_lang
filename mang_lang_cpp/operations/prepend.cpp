01926ce2187d22bf1f8f00a8341224abafacf25d
Magnus Burenius <magnus.burenius@gmail.com>
2021-08-08 23:52:14 +0200

    refactor: prepend

--- mang_lang_cpp/operations/prepend.cpp
+++ mang_lang_cpp/operations/prepend.cpp
@@ -3,24 +3,20 @@
 #include "../expressions/List.h"
 #include "../expressions/String.h"
 
-#include "../factory.h"
-
-#include "list.h"
-
-ExpressionPointer prependList(ExpressionPointer list, ExpressionPointer item) {
-    auto new_list = ::prepend(::list(list), item);
-    return makeList(std::make_shared<List>(list->range, ExpressionPointer{}, new_list));
+ExpressionPointer prependList(const List& list, ExpressionPointer item) {
+    auto new_list = ::prepend(list.elements, item);
+    return makeList(std::make_shared<List>(list.range, ExpressionPointer{}, new_list));
 }
 
-ExpressionPointer prependString(ExpressionPointer string, ExpressionPointer item) {
-    auto new_list = ::prepend(list(string), item);
-    return makeString(std::make_shared<String>(string->range, ExpressionPointer{}, new_list));
+ExpressionPointer prependString(const String& string, ExpressionPointer item) {
+    auto new_list = ::prepend(string.elements, item);
+    return makeString(std::make_shared<String>(string.range, ExpressionPointer{}, new_list));
 }
 
 ExpressionPointer prepend(ExpressionPointer expression, ExpressionPointer item) {
     switch (expression.type) {
-        case LIST: return prependList(expression, item);
-        case STRING: return prependString(expression, item);
+        case LIST: return prependList(expression.list(), item);
+        case STRING: return prependString(expression.string(), item);
         default: throw std::runtime_error{"Expected list"};
     }
 }
