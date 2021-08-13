a53523eafb5cb93c30c937546d50a577bf7da328
Magnus Burenius <magnus.burenius@gmail.com>
2021-08-14 00:00:17 +0200

    refactor: inline lookup.cpp into evaluate.cpp

--- mang_lang_cpp/operations/evaluate.cpp
+++ mang_lang_cpp/operations/evaluate.cpp
@@ -4,7 +4,6 @@
 
 #include "apply.h"
 #include "boolean.h"
-#include "lookup.h"
 #include "serialize.h"
 
 ExpressionPointer evaluateConditional(
@@ -133,6 +132,47 @@ ExpressionPointer lookupDictionary(const ExpressionPointer& expression, const st
         }
     }
     return lookupDictionary(dictionary.environment, name);
+}
+
+ExpressionPointer lookupChildInDictionary(const Dictionary& dictionary, const std::string& name) {
+    for (const auto& element : dictionary.elements) {
+        if (element.type == NAMED_ELEMENT) {
+            const auto dictionary_element = element.namedElement();
+            if (dictionary_element.name->value == name) {
+                return dictionary_element.expression;
+            }
+        }
+    }
+    throw std::runtime_error("Dictionary does not contain symbol " + name);
+}
+
+ExpressionPointer lookupChildInList(const List& list, const std::string& name) {
+    if (name == "first") {
+        return list.elements->first;
+    }
+    if (name == "rest") {
+        return makeList(new List{list.range, list.elements->rest});
+    }
+    throw std::runtime_error("List does not contain symbol " + name);
+}
+
+ExpressionPointer lookupChildInString(const String& string, const std::string& name) {
+    if (name == "first") {
+        return string.elements->first;
+    }
+    if (name == "rest") {
+        return makeString(new String{string.range, string.elements->rest});
+    }
+    throw std::runtime_error("String does not contain symbol " + name);
+}
+
+ExpressionPointer lookupChild(ExpressionPointer expression, const std::string& name) {
+    switch(expression.type) {
+        case DICTIONARY: return lookupChildInDictionary(expression.dictionary(), name);
+        case LIST: return lookupChildInList(expression.list(), name);
+        case STRING: return lookupChildInString(expression.string(), name);
+        default: throw std::runtime_error{"Cannot lookupChild expression of type " + std::to_string(expression.type)};
+    }
 }
 
 ExpressionPointer evaluateLookupChild(
--- mang_lang_cpp/operations/lookup.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "../Expression.h"
-
-ExpressionPointer lookupChild(ExpressionPointer expression, const std::string& name);
