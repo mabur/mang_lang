65b4cf8aeb8ab268f4da697b21a43ff9b900d4e0
Magnus Burenius <magnus.burenius@gmail.com>
2021-08-13 23:37:44 +0200

    refactor: add logic.cpp

---
+++ mang_lang_cpp/built_in_functions/logic.cpp
@@ -0,0 +1,47 @@
+#include "logic.h"
+
+#include "../Expression.h"
+
+#include "../operations/boolean.h"
+#include "../operations/list.h"
+
+#include "../factory.h"
+
+namespace logic {
+
+bool isTrue(ExpressionPointer x) {
+    return ::boolean(x);
+}
+
+bool isFalse(ExpressionPointer x) {
+    return !::boolean(x);
+}
+
+ExpressionPointer makeNumber(double x) {
+    return makeNumber(new Number(Number{CodeRange{}, x}));
+}
+
+ExpressionPointer boolean(ExpressionPointer in) {
+    return makeNumber(::boolean(in));
+}
+
+ExpressionPointer logic_not(ExpressionPointer in) {
+    return makeNumber(!::boolean(in));
+}
+
+ExpressionPointer all(ExpressionPointer in) {
+    const auto result = !findIf(list(in), isFalse);
+    return makeNumber(result);
+}
+
+ExpressionPointer any(ExpressionPointer in) {
+    const auto result = !!findIf(list(in), isTrue);
+    return makeNumber(result);
+}
+
+ExpressionPointer none(ExpressionPointer in) {
+    const auto result = !findIf(list(in), isTrue);
+    return makeNumber(result);
+}
+
+}
--- mang_lang_cpp/built_in_functions/logic.h
+++ mang_lang_cpp/built_in_functions/logic.h
@@ -1,47 +1,13 @@
 #pragma once
 
-#include "../Expression.h"
-
-#include "../operations/boolean.h"
-#include "../operations/list.h"
-
-#include "../factory.h"
+struct ExpressionPointer;
 
 namespace logic {
 
-bool isTrue(ExpressionPointer x) {
-    return ::boolean(x);
-}
-
-bool isFalse(ExpressionPointer x) {
-    return !::boolean(x);
-}
-
-ExpressionPointer makeNumber(double x) {
-    return makeNumber(new Number(Number{CodeRange{}, x}));
-}
-
-ExpressionPointer boolean(ExpressionPointer in) {
-    return makeNumber(::boolean(in));
-}
-
-ExpressionPointer logic_not(ExpressionPointer in) {
-    return makeNumber(!::boolean(in));
-}
-
-ExpressionPointer all(ExpressionPointer in) {
-    const auto result = !findIf(list(in), isFalse);
-    return makeNumber(result);
-}
-
-ExpressionPointer any(ExpressionPointer in) {
-    const auto result = !!findIf(list(in), isTrue);
-    return makeNumber(result);
-}
-
-ExpressionPointer none(ExpressionPointer in) {
-    const auto result = !findIf(list(in), isTrue);
-    return makeNumber(result);
-}
+ExpressionPointer boolean(ExpressionPointer in);
+ExpressionPointer logic_not(ExpressionPointer in) ;
+ExpressionPointer all(ExpressionPointer in) ;
+ExpressionPointer any(ExpressionPointer in);
+ExpressionPointer none(ExpressionPointer in);
 
 }
