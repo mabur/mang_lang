fc7dcd23256097d0967a5b82bdf219b96cdbbd57
Magnus Burenius <magnus.burenius@gmail.com>
2021-09-19 14:42:33 +0200

    Update printed text while running the interpretor executable

--- mang_lang_cpp/interpreter.cpp
+++ mang_lang_cpp/interpreter.cpp
@@ -9,18 +9,19 @@ int main(int argc,  char **argv) {
         cout << "Expected input file and output file." << endl;
         return 1;
     }
-
     const auto input_file_path = argv[1];
     const auto output_file_path = argv[2];
-    cout << "Input file: " << input_file_path << endl;
-    cout << "Output file: " << output_file_path << endl;
-
+    cout << "Reading program from " << input_file_path << " ... ";
     auto input_file = ifstream{input_file_path};
     const auto code = string{(istreambuf_iterator<char>(input_file)), istreambuf_iterator<char>()};
     input_file.close();
-
+    cout << "Done." << endl;
+    cout << "Evaluating program ... ";
     const auto result = evaluate(code);
+    cout << "Done." << endl;
+    cout << "Writing result to " << output_file_path << " ... ";
     auto output_file = ofstream{output_file_path};
     output_file << result;
     output_file.close();
+    cout << "Done." << endl;
 }
