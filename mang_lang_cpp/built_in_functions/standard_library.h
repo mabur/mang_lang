21597b51218cba66f0d3d08ae20d6b81938ae8d8
Magnus Burenius <magnus.burenius@gmail.com>
2021-10-17 11:43:19 +0200

    update implementation of all, none, any in standard library

--- mang_lang_cpp/built_in_functions/standard_library.h
+++ mang_lang_cpp/built_in_functions/standard_library.h
@@ -190,25 +190,10 @@ const std::string STANDARD_LIBRARY = R"(
     count_item = in (item list) out
         count_if!(in x out equal?(x item) list)
 
-    all = in list out not?list@{
-        list = list
-        while if list then first@list else no
-            list = rest@list
-        end
-    }
+    all = in list out not?drop_while!(in x out boolean?x list)
 
-    none = in list out not?list@{
-        list = list
-        while if list then not?first@list else no
-            list = rest@list
-        end
-    }
+    none = in list out not?drop_while!(not list)
 
-    any = in list out boolean?list@{
-        list = list
-        while if list then not?first@list else no
-            list = rest@list
-        end
-    }
+    any = in list out boolean?drop_while!(not list)
 }
 )";
