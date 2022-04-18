e4c169ededa7feafaea61e734250a81b80824650
Magnus Burenius <magnus.burenius@gmail.com>
2022-04-18 13:38:17 +0200

    refactor: test type checking for "min"

--- mang_lang_cpp/tests.cpp
+++ mang_lang_cpp/tests.cpp
@@ -464,6 +464,13 @@ int main() {
         {"pi", "3.14159265359"},
         {"tau", "6.28318530718"},
     });
+    test.evaluate_types("min", {
+        {"min![0]", "NUMBER"},
+        {"min![0 1]", "NUMBER"},
+        {"min![1 0]", "NUMBER"},
+        {"min![3 6 1]", "NUMBER"},
+        {"min![7 -3 8 -9]", "NUMBER"},
+    });
     test.evaluate("min", {
         {"min![0]", "0"},
         {"min![0 1]", "0"},
