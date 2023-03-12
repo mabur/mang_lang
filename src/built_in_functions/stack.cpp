434126a4d48b3c414edd4f5849b480c58e27f142
Magnus Burenius <magnus.burenius@gmail.com>
2023-03-12 18:26:18 +0100

    improve error handling for table_functions::take

--- src/built_in_functions/stack.cpp
+++ src/built_in_functions/stack.cpp
@@ -63,10 +63,19 @@ Expression putTyped(Expression in) {
 template<typename T>
 Expression takeTable(const T& table) {
     if (table.empty()) {
+        throw std::runtime_error("Cannot take item from empty table");
+    }
+    const auto& pair = table.begin()->second;
+    return makeEvaluatedTuple({}, EvaluatedTuple{{pair.key, pair.value}});
+}
+
+template<typename T>
+Expression takeTableTyped(const T& table) {
+    if (table.empty()) {
         return makeEvaluatedTuple(
             {},
             EvaluatedTuple{{Expression{}, Expression{}}}
-        );    
+        );
     }
     const auto& pair = table.begin()->second;
     return makeEvaluatedTuple({}, EvaluatedTuple{{pair.key, pair.value}});
@@ -91,8 +100,8 @@ Expression takeTyped(Expression in) {
     switch (in.type) {
         case EVALUATED_STACK: return getEvaluatedStack(in).top;
         case STRING: return getString(in).top;
-        case EVALUATED_TABLE: return takeTable(getEvaluatedTable(in));
-        case EVALUATED_TABLE_VIEW: return takeTable(getEvaluatedTableView(in));
+        case EVALUATED_TABLE: return takeTableTyped(getEvaluatedTable(in));
+        case EVALUATED_TABLE_VIEW: return takeTableTyped(getEvaluatedTableView(in));
         case EMPTY_STACK: return Expression{};
         case EMPTY_STRING: return Expression{CHARACTER, {}, {}};
         default: throw UnexpectedExpression(in.type, "take");
