1bc05cb4286acc0d8741a80eb979bc9ca535e192
Magnus Burenius <magnus.burenius@gmail.com>
2023-04-08 13:49:19 +0200

    refactor: re-order evaluate functions

--- src/operations/evaluate.cpp
+++ src/operations/evaluate.cpp
@@ -194,6 +194,103 @@ void booleanTypes(Expression expression) {
     }
 }
 
+bool boolean(Expression expression) {
+    switch (expression.type) {
+        case EVALUATED_TABLE: return !getEvaluatedTable(expression).empty();
+        case EVALUATED_TABLE_VIEW: return !getEvaluatedTableView(expression).empty();
+        case NUMBER: return static_cast<bool>(getNumber(expression));
+        case BOOLEAN: return getBoolean(expression);
+        case EVALUATED_STACK: return true;
+        case EMPTY_STACK: return false;
+        case STRING: return true;
+        case EMPTY_STRING: return false;
+        default: throw UnexpectedExpression(expression.type, "boolean operation");
+    }
+}
+
+size_t getIndex(Number number) {
+    if (number < 0) {
+        using namespace std;
+        throw runtime_error("Cannot have negative index: " + to_string(number));
+    }
+    return static_cast<size_t>(number);
+}
+
+Expression applyTupleIndexing(const EvaluatedTuple& tuple, Number number) {
+    const auto i = getIndex(number);
+    try {
+        return tuple.expressions.at(i);
+    }
+    catch (const std::out_of_range&) {
+        throw std::runtime_error(
+            "Tuple of size " + std::to_string(tuple.expressions.size()) +
+                " indexed with " + std::to_string(i)
+        );
+    }
+}
+
+Expression applyTableIndexing(const EvaluatedTable& table) {
+    if (table.rows.empty()) {
+        return Expression{};
+    }
+    return table.begin()->second.value;
+}
+
+Expression applyStackIndexing(EvaluatedStack stack) {
+    return stack.top;
+}
+
+Expression applyStringIndexing(String string) {
+    return string.top;
+}
+
+template<typename Vector, typename Predicate>
+bool allOfVectors(const Vector& left, const Vector& right, Predicate predicate) {
+    if (left.size() != right.size()) {
+        return false;
+    }
+    for (size_t i = 0; i < left.size(); ++i) {
+        if (!predicate(left[i], right[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool isEqual(Expression left, Expression right) {
+    const auto left_type = left.type;
+    const auto right_type = right.type;
+    if (left_type == NUMBER && right_type == NUMBER) {
+        return getNumber(left) == getNumber(right);
+    }
+    if (left_type == CHARACTER && right_type == CHARACTER) {
+        return getCharacter(left) == getCharacter(right);
+    }
+    if (left_type == BOOLEAN && right_type == BOOLEAN) {
+        return getBoolean(left) == getBoolean(right);
+    }
+    if (left_type == EMPTY_STACK && right_type == EMPTY_STACK) {
+        return true;
+    }
+    if (left_type == EVALUATED_STACK && right_type == EVALUATED_STACK) {
+        return allOfPairs(left, right, isEqual, EMPTY_STACK, getEvaluatedStack);
+    }
+    if (left_type == EMPTY_STRING && right_type == EMPTY_STRING) {
+        return true;
+    }
+    if (left_type == STRING && right_type == STRING) {
+        return allOfPairs(left, right, isEqual, EMPTY_STRING, getString);
+    }
+    if (left_type == EVALUATED_TUPLE && right_type == EVALUATED_TUPLE) {
+        return allOfVectors(
+            getEvaluatedTuple(left).expressions,
+            getEvaluatedTuple(right).expressions,
+            isEqual
+        );
+    }
+    return false;
+}
+
 Expression evaluateConditionalTypes(
     const Conditional& conditional, Expression environment
 ) {
@@ -216,6 +313,15 @@ Expression evaluateConditionalTypes(
     return left;
 }
 
+Expression evaluateConditional(
+    const Conditional& conditional, Expression environment
+) {
+    return
+        boolean(evaluate(conditional.expression_if, environment)) ?
+        evaluate(conditional.expression_then, environment) :
+        evaluate(conditional.expression_else, environment);
+}
+
 Expression evaluateIsTypes(
     const IsExpression& is_expression, Expression environment
 ) {
@@ -235,6 +341,19 @@ Expression evaluateIsTypes(
         }
     }
     return else_expression;
+}
+
+Expression evaluateIs(
+    const IsExpression& is_expression, Expression environment
+) {
+    const auto value = evaluate(is_expression.input, environment);
+    for (const auto alternative : is_expression.alternatives) {
+        const auto left_value = evaluate(alternative.left, environment);
+        if (isEqual(value, left_value)) {
+            return evaluate(alternative.right, environment);
+        }
+    }
+    return evaluate(is_expression.expression_else, environment);
 }
 
 Expression evaluateDictionaryTypes(
@@ -287,149 +406,6 @@ Expression evaluateDictionaryTypes(
         }
     }
     return result_environment;
-}
-
-size_t getIndex(Number number) {
-    if (number < 0) {
-        using namespace std;
-        throw runtime_error("Cannot have negative index: " + to_string(number));
-    }
-    return static_cast<size_t>(number);
-}
-
-Expression applyTupleIndexing(const EvaluatedTuple& tuple, Number number) {
-    const auto i = getIndex(number);
-    try {
-        return tuple.expressions.at(i);
-    }
-    catch (const std::out_of_range&) {
-        throw std::runtime_error(
-            "Tuple of size " + std::to_string(tuple.expressions.size()) +
-                " indexed with " + std::to_string(i)
-        );
-    }
-}
-
-Expression applyTableIndexing(const EvaluatedTable& table) {
-    if (table.rows.empty()) {
-        return Expression{};
-    }
-    return table.begin()->second.value;
-}
-
-Expression applyStackIndexing(EvaluatedStack stack) {
-    return stack.top;
-}
-
-Expression applyStringIndexing(String string) {
-    return string.top;
-}
-
-Expression evaluateFunctionApplicationTypes(
-    const FunctionApplication& function_application,
-    Expression environment
-) {
-    const auto function = lookupDictionary(getName(function_application.name), environment);
-    const auto input = evaluate_types(function_application.child, environment);
-    switch (function.type) {
-        case FUNCTION: return applyFunction(evaluate_types, getFunction(function), input);
-        case FUNCTION_BUILT_IN: return applyFunctionBuiltIn(getFunctionBuiltIn(function), input);
-        case FUNCTION_DICTIONARY: return applyFunctionDictionary(evaluate_types, getFunctionDictionary(function), input);
-        case FUNCTION_TUPLE: return applyFunctionTuple(evaluate_types, getFunctionTuple(function), input);
-
-        case EVALUATED_TABLE: return applyTableIndexing(getEvaluatedTable(function));
-        case EVALUATED_TUPLE: return applyTupleIndexing(getEvaluatedTuple(function), getNumber(input));
-        case EVALUATED_STACK: return applyStackIndexing(getEvaluatedStack(function));
-        case STRING: return applyStringIndexing(getString(function));
-
-        case EMPTY_STACK: return Expression{};
-        case EMPTY_STRING: return Expression{CHARACTER, {}, {}};
-
-        default: throw UnexpectedExpression(function.type, "evaluateFunctionApplicationTypes");
-    }
-}
-
-template<typename Vector, typename Predicate>
-bool allOfVectors(const Vector& left, const Vector& right, Predicate predicate) {
-    if (left.size() != right.size()) {
-        return false;
-    }
-    for (size_t i = 0; i < left.size(); ++i) {
-        if (!predicate(left[i], right[i])) {
-            return false;
-        }
-    }
-    return true;
-}
-
-bool isEqual(Expression left, Expression right) {
-    const auto left_type = left.type;
-    const auto right_type = right.type;
-    if (left_type == NUMBER && right_type == NUMBER) {
-        return getNumber(left) == getNumber(right);
-    }
-    if (left_type == CHARACTER && right_type == CHARACTER) {
-        return getCharacter(left) == getCharacter(right);
-    }
-    if (left_type == BOOLEAN && right_type == BOOLEAN) {
-        return getBoolean(left) == getBoolean(right);
-    }
-    if (left_type == EMPTY_STACK && right_type == EMPTY_STACK) {
-        return true;
-    }
-    if (left_type == EVALUATED_STACK && right_type == EVALUATED_STACK) {
-        return allOfPairs(left, right, isEqual, EMPTY_STACK, getEvaluatedStack);
-    }
-    if (left_type == EMPTY_STRING && right_type == EMPTY_STRING) {
-        return true;
-    }
-    if (left_type == STRING && right_type == STRING) {
-        return allOfPairs(left, right, isEqual, EMPTY_STRING, getString);
-    }
-    if (left_type == EVALUATED_TUPLE && right_type == EVALUATED_TUPLE) {
-        return allOfVectors(
-            getEvaluatedTuple(left).expressions,
-            getEvaluatedTuple(right).expressions,
-            isEqual
-        );
-    }
-    return false;
-}
-
-bool boolean(Expression expression) {
-    switch (expression.type) {
-        case EVALUATED_TABLE: return !getEvaluatedTable(expression).empty();
-        case EVALUATED_TABLE_VIEW: return !getEvaluatedTableView(expression).empty();
-        case NUMBER: return static_cast<bool>(getNumber(expression));
-        case BOOLEAN: return getBoolean(expression);
-        case EVALUATED_STACK: return true;
-        case EMPTY_STACK: return false;
-        case STRING: return true;
-        case EMPTY_STRING: return false;
-        default: throw UnexpectedExpression(expression.type, "boolean operation");
-    }
-}
-
-Expression evaluateConditional(
-    const Conditional& conditional, Expression environment
-) {
-    return
-        boolean(evaluate(conditional.expression_if, environment)) ?
-        evaluate(conditional.expression_then, environment) :
-        evaluate(conditional.expression_else, environment);
-}
-
-Expression evaluateIs(
-    const IsExpression& is_expression, Expression environment
-) {
-    const auto value = evaluate(is_expression.input, environment);
-    for (const auto alternative : is_expression.alternatives) {
-        const auto left_value = evaluate(alternative.left, environment);
-        if (isEqual(value, left_value)) {
-            return evaluate(alternative.right, environment);
-        }
-    }
-    return evaluate(is_expression.expression_else, environment);
 }
 
 Expression evaluateDictionary(
@@ -538,6 +514,29 @@ Expression applyStringIndexing(String string, Number number) {
     return string.top;
 }
 
+Expression evaluateFunctionApplicationTypes(
+    const FunctionApplication& function_application,
+    Expression environment
+) {
+    const auto function = lookupDictionary(getName(function_application.name), environment);
+    const auto input = evaluate_types(function_application.child, environment);
+    switch (function.type) {
+        case FUNCTION: return applyFunction(evaluate_types, getFunction(function), input);
+        case FUNCTION_BUILT_IN: return applyFunctionBuiltIn(getFunctionBuiltIn(function), input);
+        case FUNCTION_DICTIONARY: return applyFunctionDictionary(evaluate_types, getFunctionDictionary(function), input);
+        case FUNCTION_TUPLE: return applyFunctionTuple(evaluate_types, getFunctionTuple(function), input);
+
+        case EVALUATED_TABLE: return applyTableIndexing(getEvaluatedTable(function));
+        case EVALUATED_TUPLE: return applyTupleIndexing(getEvaluatedTuple(function), getNumber(input));
+        case EVALUATED_STACK: return applyStackIndexing(getEvaluatedStack(function));
+        case STRING: return applyStringIndexing(getString(function));
+
+        case EMPTY_STACK: return Expression{};
+        case EMPTY_STRING: return Expression{CHARACTER, {}, {}};
+
+        default: throw UnexpectedExpression(function.type, "evaluateFunctionApplicationTypes");
+    }
+}
 
 Expression evaluateFunctionApplication(
     const FunctionApplication& function_application,
@@ -563,6 +562,7 @@ Expression evaluateFunctionApplication(
 
 Expression evaluate_types(Expression expression, Expression environment) {
     switch (expression.type) {
+        // These are the same for types and values, and just pass through:
         case NUMBER: return expression;
         case CHARACTER: return expression;
         case BOOLEAN: return expression;
@@ -575,26 +575,32 @@ Expression evaluate_types(Expression expression, Expression environment) {
         case EVALUATED_TABLE: return expression;
         case EVALUATED_TABLE_VIEW: return expression;
 
+        // These are the same for types and values:
         case FUNCTION: return evaluateFunction(getFunction(expression), environment);
         case FUNCTION_TUPLE: return evaluateFunctionTuple(getFunctionTuple(expression), environment);
         case FUNCTION_DICTIONARY: return evaluateFunctionDictionary(getFunctionDictionary(expression), environment);
+        case LOOKUP_SYMBOL: return lookupDictionary(getName(getLookupSymbol(expression).name), environment);
 
-        case DYNAMIC_EXPRESSION: return Expression{};
-        case CONDITIONAL: return evaluateConditionalTypes(getConditional(expression), environment);
-        case IS: return evaluateIsTypes(getIs(expression), environment);
-        case DICTIONARY: return evaluateDictionaryTypes(getDictionary(expression), environment);
+        // These are different for types and values, but templated:
         case STACK: return evaluateStack(evaluate_types, expression, environment);
         case TUPLE: return evaluateTuple(evaluate_types, getTuple(expression), environment);
         case TABLE: return evaluateTable(evaluate_types, serialize_types, getTable(expression), environment);
         case LOOKUP_CHILD: return evaluateLookupChild(evaluate_types, getLookupChild(expression), environment);
+
+        // These are different for types and values:
+        case DYNAMIC_EXPRESSION: return Expression{};
+        case CONDITIONAL: return evaluateConditionalTypes(getConditional(expression), environment);
+        case IS: return evaluateIsTypes(getIs(expression), environment);
+        case DICTIONARY: return evaluateDictionaryTypes(getDictionary(expression), environment);
         case FUNCTION_APPLICATION: return evaluateFunctionApplicationTypes(getFunctionApplication(expression), environment);
-        case LOOKUP_SYMBOL: return lookupDictionary(getName(getLookupSymbol(expression).name), environment);
+        
         default: throw UnexpectedExpression(expression.type, "evaluate types operation");
     }
 }
 
 Expression evaluate(Expression expression, Expression environment) {
     switch (expression.type) {
+        // These are the same for types and values, and just pass through:
         case NUMBER: return expression;
         case CHARACTER: return expression;
         case BOOLEAN: return expression;
@@ -607,23 +613,25 @@ Expression evaluate(Expression expression, Expression environment) {
         case EVALUATED_TABLE: return expression;
         case EVALUATED_TABLE_VIEW: return expression;
 
+        // These are the same for types and values:
         case FUNCTION: return evaluateFunction(getFunction(expression), environment);
         case FUNCTION_TUPLE: return evaluateFunctionTuple(getFunctionTuple(expression), environment);
         case FUNCTION_DICTIONARY: return evaluateFunctionDictionary(getFunctionDictionary(expression), environment);
+        case LOOKUP_SYMBOL: return lookupDictionary(getName(getLookupSymbol(expression).name), environment);
 
-        case DYNAMIC_EXPRESSION: return evaluate(getDynamicExpression(expression).expression, environment);
-        case CONDITIONAL: return evaluateConditional(
-                getConditional(expression), environment);
-        case IS: return evaluateIs(getIs(expression), environment);
-        case DICTIONARY: return evaluateDictionary(
-                getDictionary(expression), environment);
+        // These are different for types and values, but templated:
         case STACK: return evaluateStack(evaluate, expression, environment);
         case TUPLE: return evaluateTuple(evaluate, getTuple(expression), environment);
         case TABLE: return evaluateTable(evaluate, serialize, getTable(expression), environment);
         case LOOKUP_CHILD: return evaluateLookupChild(evaluate, getLookupChild(expression), environment);
-        case FUNCTION_APPLICATION: return evaluateFunctionApplication(
-                getFunctionApplication(expression), environment);
-        case LOOKUP_SYMBOL: return lookupDictionary(getName(getLookupSymbol(expression).name), environment);
+
+        // These are different for types and values:
+        case DYNAMIC_EXPRESSION: return evaluate(getDynamicExpression(expression).expression, environment);
+        case CONDITIONAL: return evaluateConditional(getConditional(expression), environment);
+        case IS: return evaluateIs(getIs(expression), environment);
+        case DICTIONARY: return evaluateDictionary(getDictionary(expression), environment);
+        case FUNCTION_APPLICATION: return evaluateFunctionApplication(getFunctionApplication(expression), environment);
+        
         default: throw UnexpectedExpression(expression.type, "evaluate operation");
     }
 }
