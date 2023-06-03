712e3e81080091e1252f7120f60dd8a502cc453c
Magnus Burenius <magnus.burenius@gmail.com>
2023-06-03 12:18:49 +0200

    refactor: construction of definitions in builtInsTypes

--- src/built_in_functions/built_in_functions.cpp
+++ src/built_in_functions/built_in_functions.cpp
@@ -36,29 +36,25 @@ Expression builtIns() {
 }
 
 Expression builtInsTypes() {
-    auto definitions = Definitions{};
-    definitions.add("clear", define(container_functions::clearTyped));
-    definitions.add("put", define(container_functions::putTyped));
-    definitions.add("take", define(container_functions::takeTyped));
-    definitions.add("drop", define(container_functions::dropTyped));
-    definitions.add("get", define(container_functions::get));
-
-    definitions.add("add", define(arithmetic::FunctionNumberNumberToNumber{"add"}));
-    definitions.add("mul", define(arithmetic::FunctionNumberNumberToNumber{"mul"}));
-    definitions.add("sub", define(arithmetic::FunctionNumberNumberToNumber{"sub"}));
-    definitions.add("div", define(arithmetic::FunctionNumberNumberToNumber{"div"}));
-    definitions.add("mod", define(arithmetic::FunctionNumberNumberToNumber{"mod"}));
-
-    definitions.add("less", define(arithmetic::FunctionNumberNumberToBoolean{"less"}));
-
-    definitions.add("round", define(arithmetic::FunctionNumberToNumber{"round"}));
-    definitions.add("round_up", define(arithmetic::FunctionNumberToNumber{"round_up"}));
-    definitions.add("round_down", define(arithmetic::FunctionNumberToNumber{"round_down"}));
-    definitions.add("sqrt", define(arithmetic::FunctionNumberToNumber{"sqrt"}));
-
-    definitions.add("number", define(arithmetic::FunctionCharacterToNumber{"number"}));
-    definitions.add("character", define(arithmetic::FunctionNumberToCharacter{"character"}));
-
+    const auto definitions = Definitions{{
+        {"clear",       define(container_functions::clearTyped)},
+        {"put",         define(container_functions::putTyped)},
+        {"take",        define(container_functions::takeTyped)},
+        {"drop",        define(container_functions::dropTyped)},
+        {"get",         define(container_functions::get)},
+        {"add",         define(arithmetic::FunctionNumberNumberToNumber{"add"})},
+        {"mul",         define(arithmetic::FunctionNumberNumberToNumber{"mul"})},
+        {"sub",         define(arithmetic::FunctionNumberNumberToNumber{"sub"})},
+        {"div",         define(arithmetic::FunctionNumberNumberToNumber{"div"})},
+        {"mod",         define(arithmetic::FunctionNumberNumberToNumber{"mod"})},
+        {"less",        define(arithmetic::FunctionNumberNumberToBoolean{"less"})},
+        {"round",       define(arithmetic::FunctionNumberToNumber{"round"})},
+        {"round_up",    define(arithmetic::FunctionNumberToNumber{"round_up"})},
+        {"round_down",  define(arithmetic::FunctionNumberToNumber{"round_down"})},
+        {"sqrt",        define(arithmetic::FunctionNumberToNumber{"sqrt"})},
+        {"number",      define(arithmetic::FunctionCharacterToNumber{"number"})},
+        {"character",   define(arithmetic::FunctionNumberToCharacter{"character"})},
+    }};
     return makeEvaluatedDictionary(CodeRange{},
         EvaluatedDictionary{Expression{}, definitions}
     );
