772a0e6bb53a6cde82a4c0fcdec8bcca5c430b5e
Magnus Burenius <magnus.burenius@gmail.com>
2025-02-15 13:59:40 +0100

    refactor: use carma in DictionaryNameIndexer

--- src/dictionary_name_indexer.h
+++ src/dictionary_name_indexer.h
@@ -1,27 +1,48 @@
 #pragma once
 
-#include <unordered_map>
+#include <carma/carma_table.h>
 
 #include "expression.h"
 
+typedef struct {
+    size_t key; // Global index
+    size_t value; // Dictionary index
+    bool occupied;
+} GlobalIndexAndDictionaryIndex;
+
+typedef struct {
+    GlobalIndexAndDictionaryIndex* data;
+    size_t count;
+    size_t capacity;
+} TableDictionaryIndexFromGlobalIndex;
+
 struct DictionaryNameIndexer {
+    DictionaryNameIndexer() {
+        dictionary_index_from_global_index = {};
+    }
+    ~DictionaryNameIndexer() {
+        FREE_TABLE(dictionary_index_from_global_index);
+    }
     size_t size() const {
-        return dictionary_index_from_global_index.size();
+        size_t count = 0;
+        FOR_EACH_TABLE(it, dictionary_index_from_global_index) {
+            count++;
+        }
+        return count;
     }
     void bindName(BoundLocalName& name) {
         name.dictionary_index = getDictionaryIndex(name.global_index);
     }
 private:
     size_t getDictionaryIndex(size_t global_index) {
-        const auto it = dictionary_index_from_global_index.find(global_index);
-        if (it != dictionary_index_from_global_index.end()) {
-            return it->second;
+        size_t value = SIZE_MAX;
+        GET_KEY_VALUE(global_index, value, dictionary_index_from_global_index);
+        if (value != SIZE_MAX) {
+            return value;
         }
-        else {
-            const auto count = size();
-            dictionary_index_from_global_index[global_index] = count;
-            return count;
-        }
+        auto count = size();
+        SET_KEY_VALUE(global_index, count, dictionary_index_from_global_index);
+        return count;
     }
-    std::unordered_map<size_t, size_t> dictionary_index_from_global_index;
+    TableDictionaryIndexFromGlobalIndex dictionary_index_from_global_index;
 };
