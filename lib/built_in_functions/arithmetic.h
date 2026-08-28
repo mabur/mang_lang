26306192e31bb9110e82c189114bd4496760e507
Magnus Burenius <magnus.burenius@gmail.com>
2026-08-28 14:20:18 +0200

    refactor: remove declarations for missing functions

--- lib/built_in_functions/arithmetic.h
+++ lib/built_in_functions/arithmetic.h
@@ -3,29 +3,17 @@
 struct Expression;
 
 Expression builtInAdd(Expression in);
-Expression builtInAddTyped(Expression in);
 Expression builtInMul(Expression in);
-Expression builtInMulTyped(Expression in);
 Expression builtInSub(Expression in);
-Expression builtInSubTyped(Expression in);
 Expression builtInDiv(Expression in);
-Expression builtInDivTyped(Expression in);
 Expression builtInMod(Expression in);
-Expression builtInModTyped(Expression in);
 
 Expression builtInLess(Expression in);
-Expression builtInLessTyped(Expression in);
 
 Expression builtInSqrt(Expression in);
-Expression builtInSqrtTyped(Expression in);
 Expression builtInRound(Expression in);
-Expression builtInRoundTyped(Expression in);
 Expression builtInRoundUp(Expression in);
-Expression builtInRoundUpTyped(Expression in);
 Expression builtInRoundDown(Expression in);
-Expression builtInRoundDownTyped(Expression in);
 
 Expression builtInAsciiNumber(Expression in);
-Expression builtInAsciiNumberTyped(Expression in);
 Expression builtInAsciiCharacter(Expression in);
-Expression builtInAsciiCharacterTyped(Expression in);
